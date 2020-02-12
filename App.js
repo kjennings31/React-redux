d9aee9fa20d56c4009564519e9d20b231f2ac2c0
Kurt J <59672456+kjennings31@users.noreply.github.com>
2020-02-12 15:22:05 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          
          
         </p>
         <a
@@ -20,7 +20,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        
         </a>
        
       </header>
